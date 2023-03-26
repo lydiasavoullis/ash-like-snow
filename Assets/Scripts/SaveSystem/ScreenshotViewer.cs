2a7828b089d3287d3d5812ad86c8c871771e4a6a
Lydia <lydiasavoullis@gmail.com>
2023-03-26 18:53:51 +0100

    commit

--- Assets/Scripts/SaveSystem/ScreenshotViewer.cs
+++ Assets/Scripts/SaveSystem/ScreenshotViewer.cs
@@ -4,6 +4,8 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+
 public class ScreenshotViewer : MonoBehaviour
 {
     //this goes on every save file button
@@ -20,13 +22,19 @@ public class ScreenshotViewer : MonoBehaviour
         string filepath = Application.persistentDataPath + "/" + "saves" +"/"+ filename + ".png";
         ;
         if (File.Exists(filepath)) {
-            byte[] bytes = File.ReadAllBytes(filepath);
-            Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-            texture.filterMode = FilterMode.Trilinear;
-            texture.LoadImage(bytes);
-            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, Screen.width, Screen.height), new Vector2(0.5f, 0.0f), 1.0f);
+            try {
+                byte[] bytes = File.ReadAllBytes(filepath);
+                Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+                texture.filterMode = FilterMode.Trilinear;
+                texture.LoadImage(bytes);
+                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, Screen.width, Screen.height), new Vector2(0.5f, 0.0f), 1.0f);
 
-            image.sprite = sprite;
+                image.sprite = sprite;
+            }
+            catch (Exception e) {
+                
+            }
+            
         }
         //get screenshot by filename.png
         //set screenshot viewer to filename.png
